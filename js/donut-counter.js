2f74e47810727769ea3900ef6efba64bf602d80c
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-11-07 15:08:22 -0500

    sets up file for student solution

--- js/donut-counter.js
+++ js/donut-counter.js
@@ -1,24 +1,5 @@
-$(function(){ // When document is ready run the code below
-
-  // create variables for guest count and donut count.
-  
-
-  // add event listeners for clicking buttons to add and remove guests.
-  
-    // increment guest count when clicked.
-    
-    // update the text of the guest number when clicked.
-    
-
-  // add event listeners for clicking buttons to add and remove donuts.
-  
-    // increment donut count when clicked.
-    
-    // update the text of the donut number when clicked.
-    
-
-  // add event listner for clicks on the check button.
-  
-    // when clicked update the text to display of guests are hungry when there are not enough donuts per guest and happy when there are enough donuts.
-    
+$(function(){ 
+  var donutCounter = new DonutCounter();
 });
+
+// your code here
