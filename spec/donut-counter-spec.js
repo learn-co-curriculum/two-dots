50b412789cec2e54d1de0e991208fbe5f7b8cb36
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-11-07 12:37:02 -0500

    sets up tests

---
+++ spec/donut-counter-spec.js
@@ -0,0 +1,5 @@
+describe('DonutCounter', function() {
+  beforeEach(function() {
+    setFixtures('<body><div class="wrapper"><h1>Donut Party!</h1><h2 id="status"></h2><div id="guests"><h3>Guests: <span>0</span></h3><button>+</button><button>-</button></div><div id="donuts"><h3>Donuts: <span>0</span></h3><button>+</button><button>-</button></div><div id="check"><button>Check</button></div></div></body>');
+  });
+});
