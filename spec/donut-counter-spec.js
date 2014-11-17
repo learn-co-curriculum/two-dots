782a12a1350bd5efd42c8b44148dc44911b1893a
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-11-16 21:25:01 -0500

    specifies the check button to be clicked in tests

--- spec/donut-counter-spec.js
+++ spec/donut-counter-spec.js
@@ -39,25 +39,25 @@ describe('DonutCounter', function() {
   describe('calculating donuts to guests', function() {
     it('knows if there are too few donuts', function() {
       $('#guests button').first().click();
-      $('#check').click();
+      $('#check button').click();
       expect($('#status').text()).toBe('Guests are sad :(');
     });
 
     it('knows if there are no guests and no donuts', function() {
-      $('#check').click();
+      $('#check button').click();
       expect($('#status').text()).toBe('Invite someone you jerk!');
     });
 
     it('knows if there are no guests', function() {
       $('#donuts button').first().click();
-      $('#check').click();
+      $('#check button').click();
       expect($('#status').text()).toBe('Invite someone you jerk!');
     });
 
     it('knows if there are enough donuts', function() {
       $('#donuts button').first().click();
       $('#guests button').first().click();
-      $('#check').click();
+      $('#check button').click();
       expect($('#status').text()).toBe('Guests are having a blast!');
     });
 
@@ -68,7 +68,7 @@ describe('DonutCounter', function() {
       $('#donuts button').first().click();
       $('#donuts button').first().click();
       $('#guests button').first().click();
-      $('#check').click();
+      $('#check button').click();
       expect($('#status').text()).toBe('ARE YOU TRYING TO KILL US ALL!?');
     });
   });
