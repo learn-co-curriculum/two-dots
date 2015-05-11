2af50a9723c66ca918e52864af04cdd5de24aa67
changamanda <amanda.chang10@gmail.com>
2015-05-11 14:03:46 -0400

    Change back to setFixtures

--- spec/donut-counter-spec.js
+++ spec/donut-counter-spec.js
@@ -1,6 +1,6 @@
 describe('DonutCounter', function() {
   beforeEach(function() {
-    loadFixtures('index.html');
+    setFixtures('<body><div class="wrapper"><h1>Donut Party!</h1><h2 id="status"></h2><div id="guests"><h3>Guests: <span>0</span></h3><button>+</button><button>-</button></div><div id="donuts"><h3>Donuts: <span>0</span></h3><button>+</button><button>-</button></div><div id="check"><button>Check</button></div></div></body>');
     var donutCounter = new DonutCounter();
   });
 
